e2386dfd34f6ca20c4cbdf601daa2f54bde5355f
sosina14 <sosiayu14@gmail.com>
2025-04-07 00:48:28 +0300

    group study

--- T.py
+++ T.py
@@ -1,11 +1,16 @@
 matrix = []
 
-for i in range(3):
+for i in range(2):
     row = []
-    for j in range(3):
+    for j in range(2):
         value = int(input(f"Enter value for row {i+1}, column {j+1}: "))
         row.append(value)
     matrix.append(row)
 
+print ('  The Matrix is of user input\n')
 for r in matrix:
     print(r)
+print("\n The transpose of the matrix user intured \n")
+for n in zip(*matrix):
+    print(n)
+
