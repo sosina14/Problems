ab17938f6877a49cea456121193ae36e37081910
sosina14 <sosiayu14@gmail.com>
2025-04-11 23:41:40 +0300

    sth

--- swap.py
+++ swap.py
@@ -1,2 +1,10 @@
-def sawp():
-    
+def split_and_join(line):
+    # write your code here
+    l = line.split()
+    line ="-".join(l)
+
+    return line
+if __name__ == '__main__':
+    line = input()
+    result = split_and_join(line)
+    print(result)
