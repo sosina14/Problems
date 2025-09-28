755964cc3af65bd2d0a002db948f13c9ea6f9020
Sosina Ayele <148296605+sosina14@users.noreply.github.com>
2025-09-28 22:25:03 +0300

    Time: 4 ms (44.92%) | Memory: 19.6 MB (42.33%) - LeetSync

---
+++ 136-single-number/single-number.py
@@ -0,0 +1,8 @@
+from typing import List
+
+class Solution:
+    def singleNumber(self, nums: List[int]) -> int:
+        result = 0
+        for num in nums:
+            result ^= num   # XOR cancels out pairs, leaves the single number
+        return result
