848e1975f495158bc8a37dbe039a842878883f14
Sosina Ayele <148296605+sosina14@users.noreply.github.com>
2025-10-01 20:18:26 +0300

    Time: 11 ms (20.81%) | Memory: 18.7 MB (79.82%) - LeetSync

--- LeetCode/283-move-zeroes/move-zeroes.py
+++ LeetCode/283-move-zeroes/move-zeroes.py
@@ -3,6 +3,29 @@ class Solution:
         """
         Do not return anything, modify nums in-place instead.
         """
+        i = 0
+        j = 1
+        while j < len(nums):
+            if nums[i] == 0 and nums[j] != 0:
+                nums[i],nums[j] = nums[j] , nums[i]
+                i += 1
+            j += 1
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
         w = 0
         r = 0
         while r < len(nums):
@@ -14,6 +37,18 @@ class Solution:
                 w += 1
             r += 1 
         
+# r = 1
+# w = 0
+# 1,0,0,3,12
+# r = 2
+# w = 1
+# no change r = 3
+# w = 1
+# 1,3,0,0,12
+# w = 2
+# r = 4
+# 1,3,12,0,0
+
 
         # write = 0
         # read = 0
