9a88e61e6766eefabc255bbd874c72e80480cde2
Sosina Ayele <148296605+sosina14@users.noreply.github.com>
2025-10-01 20:20:26 +0300

    Update move-zeroes.py

--- LeetCode/283-move-zeroes/move-zeroes.py
+++ LeetCode/283-move-zeroes/move-zeroes.py
@@ -26,41 +26,6 @@ class Solution:
 
 
 
-        w = 0
-        r = 0
-        while r < len(nums):
-            if nums[r] != 0:
-                # t = nums[r]
-                # nums[r] = nums[w]
-                # nums[w] = t
-                nums[r] , nums[w] = nums[w], nums[r]
-                w += 1
-            r += 1 
-        
-# r = 1
-# w = 0
-# 1,0,0,3,12
-# r = 2
-# w = 1
-# no change r = 3
-# w = 1
-# 1,3,0,0,12
-# w = 2
-# r = 4
-# 1,3,12,0,0
-
-
-        # write = 0
-        # read = 0
-        # while read < len(nums):
-        #     if nums[read] != 0:
-        #         temp = nums[read]
-        #         nums[read] = nums[write]
-        #         nums[write] = temp
-        #         write = write + 1
-        #     read = read + 1
-
-
 
 
 
