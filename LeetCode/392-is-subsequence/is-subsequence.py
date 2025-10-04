2b8a81ae65141c8b647ac8ee4a71946b69c39eba
Sosina Ayele <148296605+sosina14@users.noreply.github.com>
2025-10-04 16:08:05 +0300

    Time: 3 ms (25.30%) | Memory: 17.7 MB (67.69%) - LeetSync

--- LeetCode/392-is-subsequence/is-subsequence.py
+++ LeetCode/392-is-subsequence/is-subsequence.py
@@ -7,10 +7,10 @@ class Solution:
         for i in range(len(arr1)):
             while j < len(arr) and arr1[i] != arr[j]:
                 j += 1
-            if j == len(arr):  # reached end without finding arr1[i]
+            if j == len(arr):  
                 break
             count += 1
-            j += 1  # move to next position in arr for next match      
+            j += 1 
         if count == len(arr1):
             return True 
         else:
