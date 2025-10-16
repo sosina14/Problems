578401f5fe06ad44c452cc1d0657ba3af9568343
Sosina Ayele <148296605+sosina14@users.noreply.github.com>
2025-10-16 09:17:52 +0300

    Time: 11 ms (62.83%) | Memory: 19.1 MB (53.44%) - LeetSync

---
+++ LeetCode/645-set-mismatch/set-mismatch.py
@@ -0,0 +1,17 @@
+class Solution:
+    def findErrorNums(self, nums: List[int]) -> List[int]:
+        seen = {}
+        dup = -1
+        miss = -1
+        for i in range(len(nums)):
+            if nums[i] in seen:
+                dup = nums[i]
+            else:
+                seen[nums[i]] = True
+
+        for i in range(1, len(nums)+1):
+            if i not in seen:
+                miss = i
+                break
+
+        return dup,miss
