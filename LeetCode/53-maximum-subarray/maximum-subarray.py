4ff9814d724e80ab62e7d99779d90d467334cc6e
Sosina Ayele <148296605+sosina14@users.noreply.github.com>
2025-11-20 16:45:24 +0300

    Time: 91 ms (30.97%) | Memory: 32.8 MB (22.45%) - LeetSync

--- LeetCode/53-maximum-subarray/maximum-subarray.py
+++ LeetCode/53-maximum-subarray/maximum-subarray.py
@@ -1,26 +1,26 @@
+# class Solution:
+#     def maxSubArray(self, nums: List[int]) -> int:
+#         i = 0
+#         j = 0
+#         summ = 0
+#         max_sum = nums[0]
+#         n = len(nums)
+#         while j < n:
+#             summ += nums[j]
+#             max_sum = max(max_sum, summ)
+#             while summ < 0 and i <= j:
+#                 summ -= nums[i]
+#                 i += 1
+#             j += 1
+
+#         return max_sum
+
+
+
 class Solution:
     def maxSubArray(self, nums: List[int]) -> int:
-        i = 0
-        j = 0
-        summ = 0
-        max_sum = nums[0]
-        n = len(nums)
-        while j < n:
-            summ += nums[j]
-            max_sum = max(max_sum, summ)
-            while summ < 0 and i <= j:
-                summ -= nums[i]
-                i += 1
-            j += 1
-
+        max_sum = curr_sum = nums[0]
+        for i in range(1, len(nums)):
+            curr_sum = max(nums[i], curr_sum + nums[i])
+            max_sum = max(max_sum, curr_sum)
         return max_sum
-
-
-
-#class Solution:
-#     def maxSubArray(self, nums: List[int]) -> int:
-#         # max_sum = curr_sum = nums[0]
-#         # for i in range(1, len(nums)):
-#         #     curr_sum = max(nums[i], curr_sum + nums[i])
-#         #     max_sum = max(max_sum, curr_sum)
-#         # return max_sum
