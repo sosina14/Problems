b4493c7d8be4611cbc66633ae556562807012f90
Sosina Ayele <148296605+sosina14@users.noreply.github.com>
2025-12-10 22:09:04 +0300

    Time: 31 ms (46.54%) | Memory: 21.1 MB (71.46%) - LeetSync

--- LeetCode/1016-subarray-sums-divisible-by-k/subarray-sums-divisible-by-k.py
+++ LeetCode/1016-subarray-sums-divisible-by-k/subarray-sums-divisible-by-k.py
@@ -12,13 +12,3 @@ class Solution:
             freq[r] = freq.get(r, 0) + 1
 
         return count
-
-# class Solution:
-#     def subarraysDivByK(self, nums: List[int], k: int) -> int:
-        # count = 0
-        # for i in range(len(nums)):
-        #     for j in range(i , len(nums)):
-        #         summ = sum(nums[i:j+1])
-        #         if summ % k == 0:
-        #             count += 1
-        # return count 
