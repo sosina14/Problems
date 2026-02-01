e9fa3fb1dfbd21369db98565ca54135659c192b4
Sosina Ayele <148296605+sosina14@users.noreply.github.com>
2026-02-01 20:34:10 +0300

    Time: 0 ms (100.00%) | Memory: 19.5 MB (15.53%) - LeetSync

--- LeetCode/14-longest-common-prefix/longest-common-prefix.py
+++ LeetCode/14-longest-common-prefix/longest-common-prefix.py
@@ -3,7 +3,7 @@ class Solution:
         min_len = float("inf")
         for i in strs:
             min_len = min(min_len, len(i))
-        resul = ""
+        r = ""
         for i in range(min_len):
             found = True
             for j in range(len(strs)):
@@ -11,7 +11,7 @@ class Solution:
                     found = False
                     break
             if found:
-                resul += strs[0][i]
+                r += strs[0][i]
             else:
                 break
-        return resul
+        return r
