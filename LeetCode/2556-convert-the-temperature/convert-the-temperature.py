8fa746c32fea35c119f86b3bc114adf15d97935f
Sosina Ayele <148296605+sosina14@users.noreply.github.com>
2026-02-03 11:44:21 +0300

    Time: 0 ms (100.00%) | Memory: 19.4 MB (40.79%) - LeetSync

--- LeetCode/2556-convert-the-temperature/convert-the-temperature.py
+++ LeetCode/2556-convert-the-temperature/convert-the-temperature.py
@@ -1,5 +1,5 @@
 class Solution:
     def convertTemperature(self, celsius: float) -> List[float]:
-        Kelvin = celsius + 273.15
+        Kelvi = celsius + 273.15
         Fahrenheit = celsius * 1.80 + 32.00
-        return [Kelvin, Fahrenheit]
+        return [Kelvi, Fahrenheit]
